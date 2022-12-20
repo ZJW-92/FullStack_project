e160afdc64576d6d53151d9fba63e21431012c95
zhijie <guszhiwe@student.gu.se>
2022-12-20 13:29:28 +0100

    create one example on client

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Grid, CircularProgress } from "@material-ui/core" ;
 import Post from "./Post/Post.js"
 import useStyles from './styles'
 import {useSelector} from 'react-redux'; 
@@ -7,19 +8,24 @@ import {useSelector} from 'react-redux';
 
 
 const Posts = () => {
-  
- const post = useSelector((state) => state.posts)
-
+ const posts = useSelector((state) => state.posts)
  const classes = useStyles(); 
 
-
-
-  const posts = "to be added";
     return (
-      <>
-      <h1>{posts}</h1>
-      <h1>{posts}</h1>
-      </>
+      !posts.length ? <CircularProgress color ="secondary" /> : 
+      ( <Grid className= {classes.container} container alignItem= "stretch" spacing={3}>
+         {  posts.map((post) => (
+          <Grid key={post._id} item xs={12} sm={6}>
+            <Post post ={post}></Post>
+          </Grid>
+         ))
+
+
+         }
+        </Grid>
+      )
+     
+   
     ); 
 }
 
