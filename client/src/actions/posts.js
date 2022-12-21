0b30ec5a16f91839d481dd0c37cedaebb343fd84
ZJW-92 <zjwei0201@gmail.com>
2022-12-21 13:24:00 +0100

    add posts.js/actions

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -1,3 +1,4 @@
+
 import * as api from '../api'; 
 
 //action creators 
@@ -10,7 +11,6 @@ export const getPosts = () => async(dispatch) =>  {
   }catch  (error) {
     console.log(error.message); 
   }
-   
 }
 
 export const createPost = (post) => async (dispatch) => {
@@ -19,6 +19,36 @@ export const createPost = (post) => async (dispatch) => {
     dispatch({type: 'CREATE', payload: data}); 
     
   } catch (error) {
-     console.log (error);  
+     console.log (error.message);  
   }
 }
+
+export const updatePost = (id, post) => async (dispatch) => {
+  try {
+    const { data } = await api.updatePost(id, post);
+
+    dispatch({ type: 'UPDATE', payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const likePost = (id) => async (dispatch) => {
+  try {
+    const { data } = await api.likePost(id);
+
+    dispatch({ type: 'LIKE', payload: data });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: 'DELETE', payload: id });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
