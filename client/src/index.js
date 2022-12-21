0367ad4c6252b419a7ec98721458b480b1d5312a
ZJW-92 <zjwei0201@gmail.com>
2022-12-21 13:26:48 +0100

    update posts.js/reducers

--- client/src/index.js
+++ client/src/index.js
@@ -6,9 +6,10 @@ import thunk from 'redux-thunk';
 
 import { reducers } from './reducers';
 import App from './App';
-
+import './index.css';  
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)));
+
 
 ReactDOM.render(
   <Provider store={store}>
