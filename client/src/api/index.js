adb5626563be6bdeb620001346ab2cc845592775
ZJW-92 <zjwei0201@gmail.com>
2022-12-21 13:24:54 +0100

    add index.js/api

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -6,3 +6,8 @@ export const fetchPosts =() => axios.get(url) ;
 
 export const createPost = (newPost) => axios.post(url, newPost); 
 
+export const updatePost = (id, updatePost) =>axios.patch(`${url}/${id}`, updatePost); 
+
+export const likePost = (id) => axios.patch(`${url}/${id}/likePost`);
+
+export const deletePost = (id) => axios.delete(`${url}/${id}`);
